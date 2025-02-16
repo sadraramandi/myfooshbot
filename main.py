64b7cd3e0cd512db648a8cd9250e46f8363431ea
sadra darzi <143597057+sadraramandi@users.noreply.github.com>
2025-02-16 19:39:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,33 @@ TOKEN = "8049027104:AAFVJAZexvEpCjXcke0lzqk3UgFnLMeTNYc"
 
 bot = telebot.TeleBot(TOKEN)
 
-# لیست جملات تصادفی
+# لیست جملات تصادفی برای همه کاربران
 responses = [
     "کم زر بزن",
     "گوه نخور",
     "اسکل",
     "میدونستی خیلی احمقی؟",
-    "جوون بچه خوشگل"
+    "جوون بچه خوشگل",
     "کم تر زر بزنی کسی نمیگه لالی",
     "چرا تو نمیمیری؟. برو زیر تریلی دیگه",
     "یه سول دارم چند تا درخت توته؟"
+]
+
+# لیست جملات خاص برای کاربر با یوزرنیم مشخص شده
+special_responses = [
+    "اوکی",
+    "چشم",
+    "حتما"
 ]
 
 # هندل پیام‌های جدید در گروه
 @bot.message_handler(func=lambda message: True)
 def send_random_message(message):
     if message.chat.type in ["group", "supergroup"]:
-        response = random.choice(responses)
+        if message.from_user.username == "sadradarziramandi":
+            response = random.choice(special_responses)
+        else:
+            response = random.choice(responses)
         bot.reply_to(message, response)
 
 print("🤖 ربات فعال شد...")
